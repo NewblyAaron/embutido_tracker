db432fad3a54ac4d6105c913d0bf572b052c27c8
Aaron M. Serrano <amserrano1337@gmail.com>
2025-07-29 21:31:55 +0800

    chore: add provider and supabase_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
+import 'package:embutido_tracker/ui/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const EmbutidoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class EmbutidoApp extends StatelessWidget {
+  const EmbutidoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: Scaffold(body: LoginScreen()));
   }
 }

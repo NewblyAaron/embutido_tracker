02a517ad113555c9c1a95bf1a7567c5fbcdeb474
Aaron M. Serrano <amserrano1337@gmail.com>
2025-07-30 07:55:40 +0800

    test: add unit tests for AuthService, Login and Register ViewModels

--- lib/core/logging/logger_access.dart
+++ lib/core/logging/logger_access.dart
@@ -4,9 +4,14 @@ import 'package:provider/provider.dart';
 
 class LoggerAccess {
   static late BuildContext _context;
+  static LoggerService? _overrideLogger;
 
   static void init(BuildContext context) => _context = context;
 
+  static void overrideLogger(LoggerService logger) => _overrideLogger = logger;
+
+  static void clearOverride() => _overrideLogger = null;
+
   static LoggerService get logger =>
-      Provider.of<LoggerService>(_context, listen: false);
+      _overrideLogger ?? Provider.of<LoggerService>(_context, listen: false);
 }
---
+++ test/ui/login/register_viewmodel_test.dart
@@ -0,0 +1,55 @@
+import 'package:embutido_tracker/core/logging/logger_access.dart';
+import 'package:embutido_tracker/domain/entity/user.dart';
+import 'package:embutido_tracker/ui/login/register_viewmodel.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../mocks/mocks.mocks.dart';
+
+void main() {
+  late MockAuthService mockAuth;
+  late MockLoggerService mockLogger;
+  late RegisterViewModel viewModel;
+
+  setUp(() {
+    mockAuth = MockAuthService();
+    mockLogger = MockLoggerService();
+
+    LoggerAccess.overrideLogger(mockLogger);
+    viewModel = RegisterViewModel(auth: mockAuth);
+  });
+
+  test(
+    'given valid credentials when signup is successful expect error is null',
+    () async {
+      final fakeUser = User(id: "123", email: "test@example.com");
+
+      when(
+        mockAuth.signUp(
+          email: anyNamed("email"),
+          password: anyNamed("password"),
+        ),
+      ).thenAnswer((_) async {
+        return fakeUser;
+      });
+
+      await viewModel.signUp(email: "test@example.com", password: "123456");
+
+      expect(viewModel.error, isNull);
+    },
+  );
+
+  test('given invalid credentials when signup failure expect error', () async {
+    when(
+      mockAuth.signUp(email: anyNamed("email"), password: anyNamed("password")),
+    ).thenThrow(Exception("Invalid signup credentials"));
+
+    await viewModel.signUp(email: "wrong@incorrect.no", password: "654321");
+
+    expect(viewModel.error, isNotNull);
+    expect(
+      viewModel.error,
+      contains("Signup failed: Invalid signup credentials"),
+    );
+  });
+}

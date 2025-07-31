9e3716f4284da722432506d17851be9e3f255a95
Aaron M. Serrano <amserrano1337@gmail.com>
2025-07-31 10:13:41 +0800

    feat: setup DI for supabase implementations

--- lib/di/providers.dart
+++ lib/di/providers.dart
@@ -1,6 +1,10 @@
 import 'package:embutido_tracker/core/services/logger_service.dart';
+import 'package:embutido_tracker/data/repositories/supabase_user_repository.dart';
 import 'package:embutido_tracker/data/sources/remote/supabase_auth_source.dart';
-import 'package:embutido_tracker/domain/repositories/auth_repository.dart';
+import 'package:embutido_tracker/data/sources/remote/supabase_user_source.dart';
+import 'package:embutido_tracker/domain/repositories/user_repository.dart';
+import 'package:embutido_tracker/domain/sources/auth_source.dart';
+import 'package:embutido_tracker/domain/sources/user_remote_source.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -9,5 +13,15 @@ List<InheritedProvider> globalProviders = [
   Provider<SupabaseClient>(create: (_) => Supabase.instance.client),
   Provider<AuthService>(
     create: (context) => SupabaseAuthSource(context.read<SupabaseClient>()),
+  ),
+  Provider<UserRemoteSource>(
+    create: (context) => SupabaseUserSource(context.read<SupabaseClient>()),
+  ),
+  Provider<UserRepository>(
+    create:
+        (context) => SupabaseUserRepository(
+          context.read<AuthService>(),
+          context.read<UserRemoteSource>(),
+        ),
   ),
 ];

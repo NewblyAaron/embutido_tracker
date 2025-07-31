78350da45ebb315f96144c12f3b92b350625a8da
Aaron M. Serrano <amserrano1337@gmail.com>
2025-07-31 10:06:10 +0800

    feat: HomeScreen

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:embutido_tracker/core/logging/logger_access.dart';
+import 'package:embutido_tracker/ui/home/map/map_page.dart';
+import 'package:embutido_tracker/ui/home/profile/profile_page.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 1;
+
+  final PageController _pageController = PageController(initialPage: 1);
+  final List<Widget> _pages = [ProfilePage(), MapPage(), Placeholder()];
+  final List<BottomNavigationBarItem> _navItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+    BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.abc),
+      label: "Placeholder",
+    ),
+  ];
+
+  void _onBottomNavBarItemTapped(int index) => setState(() {
+    LoggerAccess.logger.debug("Navigating to index $index");
+    _selectedIndex = index;
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.ease,
+    );
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          controller: _pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: _pages,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onBottomNavBarItemTapped,
+        items: _navItems,
+      ),
+    );
+  }
+}

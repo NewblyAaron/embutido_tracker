d05ab321dad96617af2b5f38cd45f636d54cb3b8
Aaron M. Serrano <amserrano1337@gmail.com>
2025-08-01 09:10:37 +0800

    fix: dependency injection

--- lib/di/providers.dart
+++ lib/di/providers.dart
@@ -1,21 +1,52 @@
+import 'package:embutido_tracker/core/services/image_service.dart';
 import 'package:embutido_tracker/core/services/logger_service.dart';
 import 'package:embutido_tracker/data/repositories/supabase_user_repository.dart';
 import 'package:embutido_tracker/data/sources/remote/supabase_auth_source.dart';
+import 'package:embutido_tracker/data/sources/remote/supabase_avatar_cache_service.dart';
+import 'package:embutido_tracker/data/sources/remote/supabase_storage_source.dart';
 import 'package:embutido_tracker/data/sources/remote/supabase_user_source.dart';
 import 'package:embutido_tracker/domain/repositories/user_repository.dart';
+import 'package:embutido_tracker/domain/services/avatar_cache_service.dart';
 import 'package:embutido_tracker/domain/sources/auth_source.dart';
 import 'package:embutido_tracker/domain/sources/user_remote_source.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-List<InheritedProvider> globalProviders = [
+final coreProviders = [
   Provider<LoggerService>(create: (_) => LoggerService()),
+  Provider<ImageService>(create: (_) => ImageService()),
+  Provider<BaseCacheManager>(create: (_) => DefaultCacheManager()),
+];
+
+final supabaseProviders = [
   Provider<SupabaseClient>(create: (_) => Supabase.instance.client),
+];
+
+final authProviders = [
   Provider<AuthService>(
     create: (context) => SupabaseAuthSource(context.read<SupabaseClient>()),
   ),
+];
+
+final userProviders = [
+  Provider<SupabaseStorageSource>(
+    create: (context) => SupabaseStorageSource(context.read<SupabaseClient>()),
+  ),
+  Provider<AvatarCacheService>(
+    create:
+        (context) => SupabaseAvatarCacheService(
+          context.read<SupabaseStorageSource>(),
+          context.read<BaseCacheManager>(),
+        ),
+  ),
   Provider<UserRemoteSource>(
-    create: (context) => SupabaseUserSource(context.read<SupabaseClient>()),
+    create:
+        (context) => SupabaseUserSource(
+          context.read<SupabaseClient>(),
+          context.read<SupabaseStorageSource>(),
+          context.read<AvatarCacheService>(),
+        ),
   ),
   Provider<UserRepository>(
     create:
@@ -23,5 +54,15 @@ List<InheritedProvider> globalProviders = [
           context.read<AuthService>(),
           context.read<UserRemoteSource>(),
         ),
+    dispose: (context, repository) {
+      if (repository is SupabaseUserRepository) repository.dispose();
+    },
   ),
+];
+
+final globalProviders = [
+  ...coreProviders,
+  ...supabaseProviders,
+  ...authProviders,
+  ...userProviders,
 ];
